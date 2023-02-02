e5ba4cb079ec248873be4eff41fad162f0ad8461
20a31a0561 <109956547+20a31a0561@users.noreply.github.com>
2023-02-02 09:15:39 +0530

    day1
    programs

---
+++ candy program.py
@@ -0,0 +1,9 @@
+kumar=int(input("enter no of sugar candys in kg: "))
+sam=kumar/2
+sam_1=sam/2
+kumar_1=sam+sam_1
+
+
+
+print("total candy Kumar having is: ",kumar_1)
+print("total candy Sam having is: ",sam_1)
